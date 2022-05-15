b925b8562504b6a16770355e2667305d1939b1fb
Marius Urbonas <59611229+Marius4568@users.noreply.github.com>
2022-05-15 15:34:36 +0300

    remove specialty from register

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-const burgerMenu = document.querySelector(".burger-menu");
-const headerNavlinks = document.querySelector("header nav ul");
+const burgerMenu = document.querySelector('.burger-menu');
+const headerNavlinks = document.querySelector('header nav ul');
 
 const navtl = gsap.timeline({ paused: true });
 
-navtl.to("header nav ul", {
+navtl.to('header nav ul', {
   opacity: 1,
-  width: "100vw",
+  width: '100vw',
 });
-navtl.set("header nav ul a", {
-  display: "block",
+navtl.set('header nav ul a', {
+  display: 'block',
 });
-navtl.to("header nav ul a", {
+navtl.to('header nav ul a', {
   stagger: 0.2,
   opacity: 1,
 });
@@ -24,14 +24,14 @@ function gsapToggle(timeline, reverseDuration) {
   }
 }
 
-burgerMenu.addEventListener("click", (ev) => {
-  burgerMenu.classList.toggle("burger-menu-x");
+burgerMenu.addEventListener('click', (ev) => {
+  burgerMenu.classList.toggle('burger-menu-x');
 
   gsapToggle(navtl, 0.5);
 });
 
-window.addEventListener("storage", (ev) => {
-  if (ev.key === "userToken" && ev.newValue === null) {
-    location.href = "login.html";
+window.addEventListener('storage', (ev) => {
+  if (ev.key === 'userToken' && ev.newValue === null) {
+    location.href = 'login.html';
   }
 });
--- src/js/register.js
+++ src/js/register.js
@@ -1,37 +1,36 @@
-const token = sessionStorage.getItem("userToken");
+const token = sessionStorage.getItem('userToken');
 
 if (token) {
-  location.href = "patients.html";
+  location.href = 'patients.html';
 }
 
 const form = document.forms.login_form;
 
-form.addEventListener("submit", async (ev) => {
+form.addEventListener('submit', async (ev) => {
   ev.preventDefault();
   userDetails = {
     name: form.children[0].children[1].value,
-    specialty: form.children[1].children[1].value,
     email: form.children[2].children[1].value,
     password: form.children[3].children[1].value,
   };
 
   const res = await fetch(
-    "https://lemon-circular-eucalyptus.glitch.me/user/register",
+    'https://lemon-circular-eucalyptus.glitch.me/user/register',
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
       },
       body: JSON.stringify(userDetails),
-    },
+    }
   );
   const data = await res.json();
-  if (data.msg === "User created") {
-    alert("User created");
+  if (data.msg === 'User created') {
+    alert('User created');
   }
 
-  if (data.msg === "User already exists") {
-    alert("User already exists");
+  if (data.msg === 'User already exists') {
+    alert('User already exists');
   }
   console.log(data);
 });
