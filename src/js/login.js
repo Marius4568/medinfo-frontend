091b5edacb8b629511607523de2e7b2f7dd7a627
Marius Urbonas <59611229+Marius4568@users.noreply.github.com>
2022-05-16 19:04:45 +0300

    404, added spinner

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,6 @@
+const submitBtn = document.querySelector('button[type =submit]');
+console.log(submitBtn);
+
 const token = sessionStorage.getItem('userToken');
 
 if (token) {
@@ -13,21 +16,36 @@ form.addEventListener('submit', async (ev) => {
     password: form.children[1].children[1].value,
   };
 
-  const res = await fetch(
-    'https://lemon-circular-eucalyptus.glitch.me/user/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(userDetails),
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
+  submitBtn.append(spinner);
+  gsap.to(submitBtn, {
+    paddingRight: '3rem',
+  });
+
+  try {
+    const res = await fetch(
+      'https://lemon-circular-eucalyptus.glitch.me/user/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(userDetails),
+      }
+    );
+    const data = await res.json();
+    if (data.token) {
+      sessionStorage.setItem('userToken', data.token);
+      location.href = '/patients.html';
     }
-  );
-  const data = await res.json();
-  if (data.token) {
-    sessionStorage.setItem('userToken', data.token);
-    location.href = '/patients.html';
     console.log(data);
+  } catch (error) {
+    console.log(error);
   }
-  console.log(data);
+
+  gsap.to(submitBtn, {
+    paddingRight: '2rem',
+  });
+  spinner.remove();
 });
